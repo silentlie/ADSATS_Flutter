9b121d524a8edac46e6fc40aeb2e46a0c728aaf8
Tuan Linh Nguyen <52875747+silentlie@users.noreply.github.com>
2024-06-05 10:13:40 +1000

    update pub

--- lib/scaffold/scaffold_widget.dart
+++ lib/scaffold/scaffold_widget.dart
@@ -1,18 +1,15 @@
-import 'package:adsats_flutter/Amplify/auth.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:adsats_flutter/scaffold/appbar_widget.dart';
 import 'package:adsats_flutter/scaffold/drawer_widget.dart';
-import 'package:provider/provider.dart';
+import 'package:adsats_flutter/Amplify/auth.dart';
 
 class MyScaffold extends StatelessWidget {
   const MyScaffold({super.key, required this.child});
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    if (rememberDevice) {
-      rememberCurrentDevice();
-    }
     return ChangeNotifierProvider(
       create: (context) => AuthNotifier(),
       child: Scaffold(
