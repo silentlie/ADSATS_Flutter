0f36c128b55ea50b3f2b7b3e1254c38327856eef
Tuan Linh Nguyen <52875747+silentlie@users.noreply.github.com>
2024-06-06 22:37:50 +1000

    finish merge

--- lib/route/sms_route/send_notices/hazard_report_form.dart
+++ lib/route/sms_route/send_notices/hazard_report_form.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'recepients.dart';
 
 class HazardReport extends StatelessWidget {
