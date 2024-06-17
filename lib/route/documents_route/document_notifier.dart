71ea01eabd44f340e055c5a41994211459e8e475
calangetafe <163232522+calangetafe@users.noreply.github.com>
2024-06-17 23:06:47 +1000

    Add options to document upload with Content-Type to attempt to get pdf and images to open in a new browser tab. Code is close but is currently creating a user-defined metadata instead of updating the system defined Content-Type key-value pair

--- lib/route/documents_route/document_notifier.dart
+++ lib/route/documents_route/document_notifier.dart
@@ -19,13 +19,25 @@ class DocumentNotifier extends ChangeNotifier {
       String jsonStr = response.decodeBody();
       int documentID = jsonDecode(jsonStr);
       debugPrint("document_id: $documentID");
+
       String pathStr = "${documentID}_${file.name}";
+      Map<String, String>? metadata = getMetadata(pathStr);
+
+      debugPrint(getMetadata(pathStr).toString());
+
+      final options = metadata != null
+          ? StorageUploadFileOptions(
+              metadata: metadata,
+            )
+          : const StorageUploadFileOptions();
+
       final result = await Amplify.Storage.uploadFile(
         localFile: AWSFile.fromStream(
           file.readStream!,
           size: file.size,
         ),
         path: StoragePath.fromString(pathStr),
+        options: options,
         onProgress: (progress) {
           // debugPrint('Fraction completed: ${progress.fractionCompleted}');
         },
@@ -42,6 +54,28 @@ class DocumentNotifier extends ChangeNotifier {
   }
 
   Future<void> uploadFiles() async {
-  await Future.wait(filePickerResult!.files.map((file) => uploadFile(file)));
+    await Future.wait(filePickerResult!.files.map((file) => uploadFile(file)));
+  }
 }
+
+// Function to get metadata based on file type
+Map<String, String>? getMetadata(String fileName) {
+  String extension = p.extension(fileName).toLowerCase();
+
+  switch (extension) {
+    case '.png':
+      return {'Content-Type': 'image/png'};
+    case '.svg':
+      return {'Content-Type': 'image/svg+xml'};
+    case '.jpg':
+    case '.jpeg':
+      return {'Content-Type': 'image/jpeg'};
+    case '.pdf':
+      return {'Content-Type': 'application/pdf'};
+    case '.doc':
+    case '.docx':
+      return {'Content-Type': 'application/msword'};
+    default:
+      return null;
+  }
 }
